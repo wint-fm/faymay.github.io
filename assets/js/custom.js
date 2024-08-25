e61af1dc88cd6c9d6747cbddb362b75f8f50a873
wint <faymay4evermarvelous@gmail.com>
2024-08-25 10:56:34 +0700

    updated carousel arrows

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     if (typeof jQuery !== 'undefined' && typeof jQuery.fn.slick !== 'undefined') {
         jQuery('.carousel').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        adaptiveHeight: true
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: true,
+            arrows: true, // Ensure arrows are enabled
+            responsive: [
+            {
+                breakpoint: 736,
+                settings: {
+                arrows: false, // Hide arrows on smaller screens
+                }
+            }
+            ]
         });
-    } else {
-        console.error('jQuery or Slick carousel not loaded');
+        } else {
+            console.error('jQuery or Slick carousel not loaded');
     }
 });
